08830a9b966aeb0fcc093819294cfabd102b4550
Emilio Zalva <emizalva08@gmail.com>
2025-08-12 19:30:46 -0300

    ultimos cambios

---
+++ Class&funcion/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_de_inventario.Class_funcion
+{
+    internal class Helper
+    {
+
+    }
+}

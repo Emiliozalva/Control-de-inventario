fc1157ba339bfecfe4162711397a52cf5bdfca08
Emilio Zalva <emizalva08@gmail.com>
2025-08-22 09:57:21 -0300

    REspaldo por las dudads

--- Forms/EliminarProducto.cs
+++ Forms/EliminarProducto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Control_de_inventario.Class_funcion;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 
 namespace Control_de_inventario.Forms
@@ -15,11 +17,17 @@ namespace Control_de_inventario.Forms
         public EliminarProducto()
         {
             InitializeComponent();
+            checkBox1.Checked = true;
+            checkBox2.Checked = false;
+
         }
 
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox2.Checked)
+            {
+                checkBox1.Checked = false; 
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -39,12 +47,42 @@ namespace Control_de_inventario.Forms
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBox1.Checked)
+            {
+                checkBox2.Checked = false; 
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string _id = this.textBox1.Text;
+            string _name = this.textBox2.Text;
+            string _cantidad = this.textBox3.Text;
+            int id, cant; 
+            Base_de_Datos bd = new Base_de_Datos();
+            List<Producto> lista = bd.ObtenerProductos();
+            if (checkBox1.Checked)
+            {
+               if(int.TryParse(_id, out id) && int.TryParse(_cantidad, out cant))
+                {
+                    foreach (Producto p in lista) { 
+                    
+                    if(id == p.getId()) 
+                        {
+                            if (cant >= p.getStock())
+                            {
+                                bd.AgregarStockProducto(); /// SEGUIR DESDE ACA 
+                            }
+                        }
+                    
+                    }
+                }
+            }
         }
     }
 }
